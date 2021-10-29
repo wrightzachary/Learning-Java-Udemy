47d1e5ecbcbe8da4524829850b6da05e51177878
Zachary <taylorzw96@gmail.com>
2021-10-29 17:58:28 -0500

    Adding Boolean and Char work from course

---
+++ ChatAndBoolean/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar =  'D'; // chars can only store single characters
+        char myUnicode = '\u0044'; // unicode for char datatype
+        char copyrightUnicode = '\u00A9'; // copyright unicode
+
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+        System.out.println(copyrightUnicode);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOVerTwentyOne = true;
+        
+    }
+}
