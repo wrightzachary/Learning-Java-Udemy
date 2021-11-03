368a52a11eb734fa7c985ba0e772bb40fcb41560
Zachary <taylorzw96@gmail.com>
2021-11-03 01:08:12 -0500

    Adding alternate way to solve this challenge

--- MegaBytesConverter/src/MegaBytesConverter.java
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -2,22 +2,12 @@ public class MegaBytesConverter {
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
 
-        // invalid options
-        if (kiloBytes < 0) {
+        if (kiloBytes < 0){
             System.out.println("Invalid Value");
-            return;
+        } else {
+            int megabytes = (kiloBytes/1024);
+            int kiloRemainder = kiloBytes%1024;
+            System.out.println(kiloBytes+" KB = "+megabytes+" MB and "+kiloRemainder+" KB");
         }
-
-        // variables
-        int megaBytes = 0; // store megabytes converted to kilobytes
-        int remainder = 0; // remainder of kilobytes
-
-        // calculations 1 MB = 1024 KB
-        megaBytes = kiloBytes / 1024; // conversion equation
-        remainder = kiloBytes % 1024; // remainder
-
-        // output
-        System.out.println(kiloBytes + " KB " + megaBytes + " MB and " + remainder + " KB");
-
     }
 }
