6ae3d4e26a1e3658f9f611ea10f0e3bb136a2721
Zachary <taylorzw96@gmail.com>
2021-11-02 23:53:41 -0500

    Adding alternative way to conduct the return statements

--- KeywordsAndExpressions/src/com/company/Main.java
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -42,14 +42,24 @@ public class Main {
 
     public static int calculateHighScorePosition(int playerScore) {
 
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100){
+//            return 3;
+//        } else {
+//            return 4;
+//        }
+        int position = 4;
         if (playerScore >= 1000) {
-            return 1;
+            position =1;
         } else if (playerScore >= 500) {
-            return 2;
-        } else if (playerScore >= 100){
-            return 3;
-        } else {
-            return 4;
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
         }
+
+        return position;
     }
 }
